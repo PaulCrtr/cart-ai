85cc876a128f47416d12002404141754138c373b
paul <paul.th.cartier@gmail.com>
2024-12-04 19:51:09 +0100

    move stream output logic into workflow service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { HumanMessage } from '@langchain/core/messages';
 import { WorkflowService } from './workflow/workflow.service';
 
 @Injectable()
@@ -7,51 +6,7 @@ export class AppService {
   constructor(private workflowService: WorkflowService) {}
 
   async invoke(query: string): Promise<string> {
-    const streamResults = await this.workflowService.graph.stream(
-      {
-        messages: [
-          new HumanMessage({
-            content: query,
-          }),
-        ],
-      },
-      { recursionLimit: 20 },
-    );
-
-    let lastMessageContent = '';
-
-    const prettifyOutput = (output: Record<string, any>) => {
-      const keys = Object.keys(output);
-      const firstItem = output[keys[0]];
-
-      if ('messages' in firstItem && Array.isArray(firstItem.messages)) {
-        const lastMessage = firstItem.messages[firstItem.messages.length - 1];
-        lastMessageContent = lastMessage.content;
-
-        console.dir(
-          {
-            type: lastMessage._getType(),
-            content: lastMessage.content,
-            tool_calls: lastMessage.tool_calls,
-          },
-          { depth: null },
-        );
-      }
-
-      if ('sender' in firstItem) {
-        console.log({
-          sender: firstItem.sender,
-        });
-      }
-    };
-
-    for await (const output of streamResults) {
-      if (!output?.__end__) {
-        prettifyOutput(output);
-        console.log('----');
-      }
-    }
-
-    return lastMessageContent;
+    const streamResults = await this.workflowService.processStream(query);
+    return streamResults;
   }
 }
--- src/workflow/workflow.service.ts
+++ src/workflow/workflow.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ChatOpenAI } from '@langchain/openai';
 import { START, StateGraph } from '@langchain/langgraph';
 import { END, Annotation } from '@langchain/langgraph';
-import { BaseMessage } from '@langchain/core/messages';
+import { BaseMessage, HumanMessage } from '@langchain/core/messages';
 import { ResearcherService } from '../agents/researcher/researcher.service';
 import { CartHandlerService } from '../agents/cartHandler/cartHandler.service';
 import { SupervisorService } from '../agents/supervisor/supervisor.service';
@@ -31,7 +31,7 @@ export class WorkflowService {
   });
 
   private members = ['cart_handler', 'researcher'] as const;
-  public graph: any;
+  private graph: any;
 
   constructor(
     private cartHandlerService: CartHandlerService,
@@ -55,5 +55,54 @@ export class WorkflowService {
     workflow.addEdge(START, 'supervisor');
 
     this.graph = workflow.compile();
+  }
+
+  async processStream(query: string): Promise<string> {
+    const streamResults = await this.graph.stream(
+      {
+        messages: [
+          new HumanMessage({
+            content: query,
+          }),
+        ],
+      },
+      { recursionLimit: 20 },
+    );
+
+    let lastMessageContent = '';
+
+    const prettifyOutput = (output: Record<string, any>) => {
+      const keys = Object.keys(output);
+      const firstItem = output[keys[0]];
+
+      if ('messages' in firstItem && Array.isArray(firstItem.messages)) {
+        const lastMessage = firstItem.messages[firstItem.messages.length - 1];
+        lastMessageContent = lastMessage.content;
+
+        console.dir(
+          {
+            type: lastMessage._getType(),
+            content: lastMessage.content,
+            tool_calls: lastMessage.tool_calls,
+          },
+          { depth: null },
+        );
+      }
+
+      if ('sender' in firstItem) {
+        console.log({
+          sender: firstItem.sender,
+        });
+      }
+    };
+
+    for await (const output of streamResults) {
+      if (!output?.__end__) {
+        prettifyOutput(output);
+        console.log('----');
+      }
+    }
+
+    return lastMessageContent;
   }
 }
