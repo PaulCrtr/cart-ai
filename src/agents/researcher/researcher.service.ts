17c3b462e569bac7377062cc17cf850c7eac73a6
paul <paul.th.cartier@gmail.com>
2024-12-05 13:32:56 +0100

    update researcher prompt

--- src/agents/researcher/researcher.service.ts
+++ src/agents/researcher/researcher.service.ts
@@ -15,9 +15,10 @@ export class ResearcherService {
       tools: [new TavilySearchResults({ maxResults: 5 })],
       messageModifier: new SystemMessage(
         `You're a web researcher. You may use the Tavily search engine to search the web for a product. ` +
-          `Make sure to search for a product (not articles, comparison sites, or anything else). ` +
-          `If you don't have a lot of information about a product, do the search anyway. Always run a single search. ` +
-          `Don't worry about adding it to the cart; another agent is responsible for saving the information you return.`,
+          `Make sure to search for a product (not comparison sites, wikipedia, or anything else). ` +
+          `If you don't have a lot of information about a product, do the search anyway. ` +
+          `Prioritize and execute the add or search request if the query contains multiple requests.` +
+          `Don't worry about adding the product to the cart; another agent is responsible for saving the information you return.`,
       ),
     });
 
